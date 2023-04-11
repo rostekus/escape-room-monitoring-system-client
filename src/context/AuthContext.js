de8aff32e67c69ef7d16db4e9598707e7dbbe97a
Rostyslav Mosorov <34031791+rostekus@users.noreply.github.com>
2023-04-11 18:46:14 +0200

    feat: added auth context

---
+++ src/context/AuthContext.js
@@ -0,0 +1,107 @@
+import { createContext, useState, useEffect } from 'react'
+import jwt_decode from "jwt-decode";
+import { useHistory } from 'react-router-dom'
+
+const AuthContext = createContext()
+
+export default AuthContext;
+
+
+export const AuthProvider = ({children}) => {
+    let [authTokens, setAuthTokens] = useState(()=> localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null)
+    let [user, setUser] = useState(()=> localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null)
+    let [loading, setLoading] = useState(true)
+
+    const history = useHistory()
+
+    let loginUser = async (e )=> {
+        e.preventDefault()
+        let response = await fetch('http://127.0.0.1:8080/api/v1/auth/authenticate', {
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({'email':e.target.username.value, 'password':e.target.password.value})
+        })
+        
+        
+        if(response.status === 200){
+            let data = await response.json()
+            setAuthTokens(data)
+            setUser(jwt_decode(data.access_token))
+            console.log(jwt_decode(data.access_token))
+            localStorage.setItem('authTokens', JSON.stringify(data))
+            history.push('/')
+        }else{
+            alert('Something went wrong!')
+        }
+    }
+
+
+    let logoutUser = () => {
+        setAuthTokens(null)
+        setUser(null)
+        localStorage.removeItem('authTokens')
+        history.push('/login')
+    }
+
+
+    let updateToken = async ()=> {
+
+        let response = await fetch('http://127.0.0.1:8080/api/v1/auth/refresh-token', {
+            method:'POST',
+            headers:{
+                'Authorization': `Bearer ${authTokens?.refresh_token}`,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+                
+           
+        })
+
+        
+        
+        if (response.status === 200){
+            let data = await response.json()
+            setAuthTokens(data)
+            setUser(jwt_decode(data.access_token))
+            localStorage.setItem('authTokens', JSON.stringify(data))
+        }else{
+            logoutUser()
+        }
+
+        if(loading){
+            setLoading(false)
+        }
+    }
+
+    let contextData = {
+        user:user,
+        authTokens:authTokens,
+        loginUser:loginUser,
+        logoutUser:logoutUser,
+    }
+
+
+    useEffect(()=> {
+
+        if(loading){
+            updateToken()
+        }
+
+        let fourMinutes = 1000 * 60*4
+
+        let interval =  setInterval(()=> {
+            if(authTokens){
+                updateToken()
+            }
+        }, fourMinutes)
+        return ()=> clearInterval(interval)
+
+    }, [authTokens, loading])
+
+    return(
+        <AuthContext.Provider value={contextData} >
+            {loading ? null : children}
+        </AuthContext.Provider>
+    )
+}
