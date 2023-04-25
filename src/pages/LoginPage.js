84470c2a41add03cab4c13ab03513796078d7510
Rostyslav Mosorov <34031791+rostekus@users.noreply.github.com>
2023-04-25 10:58:47 +0200

    refactor: deteled css in login page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,31 +1,20 @@
 import React, {useContext} from 'react'
+import { Link } from 'react-router-dom'
 import AuthContext from '../context/AuthContext'
-
 const LoginPage = () => {
     let {loginUser} = useContext(AuthContext)
     return (
-      <div className="flex justify-center items-center h-screen">
-        <form onSubmit={loginUser} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2" htmlFor="username">
-              Username
-            </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" name="username" placeholder="Enter Username" />
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-700 font-bold mb-2" htmlFor="password">
-              Password
-            </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" name="password" placeholder="Enter Password" />
-          </div>
-          <div className="flex items-center justify-between">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-              Sign In
-            </button>
-          </div>
-        </form>
-      </div>
+        <div>
+         <div>
+            <Link to="/register" >Register</Link>
+            </div> 
+            <form onSubmit={loginUser}>
+                <input type="text" name="email" placeholder="Enter Email" />
+                <input type="password" name="password" placeholder="Enter Password" />
+                <input type="submit"/>
+            </form>
+        </div>
     )
-  }
-  
+}
+
 export default LoginPage
