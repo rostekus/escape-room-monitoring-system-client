27a5019897429adb233ea1be61dc3cb6bcefd61d
Aleksandra Mikołajczyk <242223@edu.p.lodz.pl>
2023-05-15 19:12:45 +0200

    feat: added css for HintsPage

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
-import React, {useContext} from 'react'
-import { Link } from 'react-router-dom'
-import AuthContext from '../context/AuthContext'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 
 const Header = () => {
+  return (
+    <div className="navbar">
+      <Link to="/" className="navbar-object">
+        Home
+      </Link>
 
-    return (
-        <div>
-            <Link to="/" >Home</Link>
-            <span> | </span>
+      {/* <span> | </span> */}
+    </div>
+  );
+};
 
-           
-        </div>
-    )
-}
-
-export default Header
+export default Header;
--- src/pages/HintsPage.js
+++ src/pages/HintsPage.js
@@ -1,19 +1,21 @@
-import React  from "react";
-import { getHintsForGameID } from "../api/Mock.js"
+import React from "react";
+import { getHintsForGameID } from "../api/Mock.js";
 import { useParams } from "react-router-dom";
 
 const HintsPage = () => {
-    const { gameId } = useParams();
-    console.log(gameId)
+  const { gameId } = useParams();
+  console.log(gameId);
   return (
     <div>
-    <h1>Game ID: {gameId}</h1>
-    <h1>Hints</h1>
-    <ul>
-      {getHintsForGameID(parseInt(gameId)).map((hint) => (
-        <li key={hint.num}>{hint.text}</li>
-      ))}
-    </ul>
+      <h1 className="title-hits-page">Game ID: {gameId}</h1>
+      <section className="section-hints-page">
+        <h1 className="subtitle-hints-page">Hints</h1>
+        <ul>
+          {getHintsForGameID(parseInt(gameId)).map((hint) => (
+            <li key={hint.num}>{hint.text}</li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 };
