b6d5cba5e763c697b61de6c57b6e756f4115cbc0
Marek Kopania <234760@edu.p.lodz.pl>
2023-06-08 12:15:52 +0200

    Added play hint button and prototype of functionality

--- src/pages/HintRevealPage.js
+++ src/pages/HintRevealPage.js
@@ -2,9 +2,27 @@ import React from "react";
 import {getHintsForGameID} from "../api/Mock";
 import {mapCodes} from "../api/Mock";
 import {useHistory} from "react-router-dom";
+import {Howl} from "howler";
 
 const HintRevealPage = () => {
     const history = useHistory();
+    const audioClips = [
+    { sound: "http://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg" }
+    ]
+
+    function soundPlay(src) {
+        const sound = new Howl ({
+            src,
+            html5: true
+        })
+        sound.play();
+    }
+
+    function soundOnButtonClick() {
+        audioClips.map((soundObj) => {
+          soundPlay(soundObj.sound);
+        })
+    }
     function pushToPlayerPage() {
         history.push('/player/hints');
     }
@@ -25,6 +43,7 @@ const HintRevealPage = () => {
             <textarea style={{textAlign: "center", justifyContent: "center"}} className="text_field" defaultValue={getHint()}>
             </textarea>
             <button  className="button" onClick={() => pushToPlayerPage()} >Return</button>
+            <button className="button" onClick={() => soundOnButtonClick()}>Play Hint</button>
         </div>
     )
 }
